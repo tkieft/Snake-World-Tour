9a19f5bc96f7257ab089794e797bdc1519d21f14
tkieft <tkieft@2d1a4ccd-f515-0410-a99d-d1a2ee3908d6>
2006-06-12 23:29:52 +0000

    SDL skeleton in main.cpp, add load_image.cpp and load_image.h, now loads background and displays on screen
    git-svn-id: file:///Users/tkieft/code/SVN/snake/trunk@5 2d1a4ccd-f515-0410-a99d-d1a2ee3908d6

---
+++ Snake/main.cpp
@@ -0,0 +1,101 @@
+/* 
+    Tyler Kieft
+    June 12, 2006
+    main.cpp - Initialize SDL
+*/
+   
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <cmath>
+#include "SDL.h"
+#include "load_image.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+// Function prototypes
+void init();
+void deinit();
+
+// Global variables
+const int SCREENWIDTH = 640;
+const int SCREENHEIGHT = 480;
+const int SCREENBPP = 32;
+
+SDL_Surface* screen = NULL;
+SDL_Surface* bg = NULL;
+SDL_Event event;
+
+int main(int argc, char *argv[])
+{
+    
+    string rsrcdirectory = (string) *argv;
+    rsrcdirectory = rsrcdirectory.substr( 0, rsrcdirectory.length() - 11 ) + "Resources/";
+    //cout << rsrcdirectory;
+    
+    init();
+    
+    bg = load_image( rsrcdirectory + "snake.png" );
+    //cout << bg->w << bg->h;
+    SDL_Rect offset;
+    offset.x = 0; offset.y = 0;
+    SDL_BlitSurface( bg, NULL, screen, &offset );
+    SDL_UpdateRect(screen, 0, 0, 640, 480 );
+	
+    int done = 0;
+    while ( !done ) {
+
+		/* Check for events */
+		while ( SDL_PollEvent(&event) ) {
+			switch (event.type) {
+
+				case SDL_MOUSEMOTION:
+					break;
+				case SDL_MOUSEBUTTONDOWN:
+					break;
+				case SDL_KEYDOWN:
+					/* Any keypress quits the app... */
+				case SDL_QUIT:
+					done = 1;
+					break;
+				default:
+					break;
+			}
+		}
+	}
+	
+	return(0);
+}
+
+void init()
+{
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        cout << "Unable to initialize SDL: " << SDL_GetError() << endl;
+        exit(1);
+    }
+    
+    atexit( deinit );
+    
+    // Attempt to create a 640x480 window with 32 bit pixels
+    screen = SDL_SetVideoMode( SCREENWIDTH, SCREENHEIGHT, SCREENBPP, SDL_SWSURFACE );
+    
+    if( screen == NULL )
+    {
+        cout << "Unable to set 640x480 video: " << SDL_GetError() << endl;
+        exit(1);
+    }
+    
+    // Set window title
+    SDL_WM_SetCaption("Snake v0.1", NULL);
+}
+
+void deinit()
+{
+    SDL_FreeSurface( bg );
+    /* Clean up the SDL Library */
+    SDL_Quit();
+    cout << "Quitting...";
+}
